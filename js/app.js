a6ed07de9e8aeaf78096af5e8ffca85c5feaa21e
Xarikleia Niouraki <niouraki@gmail.com>
2018-05-26 20:15:49 +0300

    Add the main arrays and the way to randomly pick values

--- js/app.js
+++ js/app.js
@@ -18,3 +18,23 @@ window.onclick = function(event) {
     }
   }
 }
+
+//The arrays with our main categories
+var mySpecies = ["Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling"];
+var myClass = ["Barbarian", "Bard", "Druid", "Monk", "Paladin", "Ranger", "Rogue", "Fighter", "Sorcerer", "Warlock", "Wizard"];
+var myBackground = ["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Hermit", "Noble", "Outlander", "Sage", "Soldier", "Urchin"];
+var myAlignment = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"];
+
+//Gets the button that rolls the dice
+var mainButton = document.querySelector(".roll-button");
+
+mainButton.addEventListener("click", function() {
+  var firstRandom = mySpecies[Math.floor(Math.random() * mySpecies.length)];
+  console.log(firstRandom);
+  var secondRandom = myClass[Math.floor(Math.random() * myClass.length)];
+  console.log(secondRandom);
+  var thirdRandom = myBackground[Math.floor(Math.random() * myBackground.length)];
+  console.log(thirdRandom);
+  var fourthRandom = myAlignment[Math.floor(Math.random() * myAlignment.length)];
+  console.log(fourthRandom); 
+});
